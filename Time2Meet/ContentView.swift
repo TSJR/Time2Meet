2513dc3c9c04b4e4a5c6f5765c1ac297545d5e99
TSJR <3ddude41@gmail.com>
2021-12-10 15:11:42 -0500

    Initial Commit

---
+++ Time2Meet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Time2Meet
+//
+//  Created by Gerald Negvesky on 12/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Time2Meet/Time2MeetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time2MeetApp.swift
+//  Time2Meet
+//
+//  Created by Gerald Negvesky on 12/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Time2MeetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
